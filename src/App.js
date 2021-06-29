4714c3dbef11b546096d82c92dfc5074f5a2d116
shubhamPassi <shubhamk0@gmail.com>
2021-06-29 08:39:40 +0530

    update menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import { Switch, Route } from "react-router-dom";
+
+const hatsPage = (props) => {
+    console.log(props);
+    return (
+        <div>
+            <h1>Hats Page</h1>
+        </div>
+    );
+};
 
 function App() {
     return (
         <div>
-            <HomePage />
+            <Switch>
+                <Route path="/hats" component={hatsPage} />
+                <Route path="/" component={HomePage} />
+            </Switch>
         </div>
     );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,19 +1,26 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, id, size }) => (
-    <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, id, size, history, linkUrl, match }) => {
+    console.log(match, linkUrl);
+    return (
         <div
-            style={{
-                backgroundImage: `url(${imageUrl})`,
-            }}
-            className="background-image"
-        ></div>
-        <div className="content">
-            <h1 className="title">{title.toUpperCase()}</h1>
-            <span className="subtitle">SHOP NOW</span>
+            className={`${size} menu-item`}
+            onClick={() => history.push(`${match.url}${linkUrl}`)}
+        >
+            <div
+                style={{
+                    backgroundImage: `url(${imageUrl})`,
+                }}
+                className="background-image"
+            ></div>
+            <div className="content">
+                <h1 className="title">{title.toUpperCase()}</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
-export default MenuItem;
+export default withRouter(MenuItem);
